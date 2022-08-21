7879afab9fcccddafea54cf19a4e09c600b270e7
Aayush Solanki <aayushsolanki40@gmail.com>
2022-08-21 11:34:48 +0530

    updated login page

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,6 +12,89 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
+            child: Image.asset('images/loginback.jpg', fit: BoxFit.cover)
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.all(20),
+            color: Color.fromRGBO(0, 0, 255, 0.2),
+            child: SingleChildScrollView(
+              child: Form(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      cursorColor: Colors.white,
+                      style: TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Email',
+                          hintStyle: TextStyle(color: Colors.white),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 1.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 1.0),
+                          )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      cursorColor: Colors.white,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      style: TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Password',
+                        hintStyle: TextStyle(color: Colors.white),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 1.0),
+                        )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                        width: double.infinity, // <-- match_parent
+                        child: ElevatedButton(style: ElevatedButton.styleFrom(
+                            primary: Colors.brown
+                        ),
+                            onPressed: () {  },
+                            child: Text('Login In'))),
+                    Text('or'),
+                    SizedBox(
+                      width: double.infinity, // <-- match_parent
+                      child: ElevatedButton(style: ElevatedButton.styleFrom(
+                        primary: Colors.brown
+                      ),
+                      onPressed: () {  },
+                      child: Text('Sign Up'))),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
